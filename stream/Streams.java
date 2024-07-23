0cc94828ae7456d66efeb6097ee28573c1f12197
babu.kiran80 <babu.kiran80@gmail.com>
2024-07-23 16:38:52 -0500

    Streams testing on Strings

--- stream/Streams.java
+++ stream/Streams.java
@@ -17,37 +17,58 @@ public class Streams {
 				Arrays.asList("3", "", "", "", "turky"), Arrays.asList("3", "", "", "", "china"),
 				Arrays.asList("3", "", "", "", "china"));
 
-		System.out.println("** " + countries);
-		 printCountOfCountries(countries);
+			System.out.println("** " + countries);
+		
+			printCountOfCountries(countries);
 		 
 		 
-			String s = "Hello, How are you";
-			
-			char ch = 'H';
-			long count = s.chars().filter(i->i==ch).count();
-			
-			System.out.println("Count of a character '" + ch + "' in the string '"+s+"' : " + count);
-			
-	        Map<Character, Long> charCount = s.chars()
-	                .mapToObj(c -> (char) c) // Convert int stream to char stream
-	                .collect(Collectors.groupingBy(
-	                        Function.identity(), // Group by the characters themselves
-	                        Collectors.counting() // Count the occurrences of each character
-	                ));
-		 
-	        System.out.println("Count of all characters in the string '"+s+"' : " + charCount);
+			printCountOfRepeatedCharsInString();
 	        
 	        
-	        String words = "Hello alan i am here where are you"+"and what are you doing hello are you there";
-	        
-	        //Arrays.asList(words.split(" ")).stream().map(String::toUpperCase).forEach(System.out::println);
-	        
-	        Map<String, Long> countOfWords = Arrays.asList(words.split(" ")).stream()
-	        		.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-	        
-	        System.out.println("Count of all words in the string '"+s+"' : " + countOfWords);
+	        printCountOfRepeatedWordsInString();
 	        
 		 
+	}
+
+
+	private static void printCountOfRepeatedWordsInString() {
+		String words = "Hello alan i am here where are you and what are you doing ? hello are you there ?";
+		
+		//Arrays.asList(words.split(" ")).stream().map(String::toUpperCase).forEach(System.out::println);
+		
+		Map<String, Long> countOfWords = Arrays.asList(words.split(" ")).stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		
+		System.out.println("Count of all words in the string '"+words+"' : " + countOfWords);
+	}
+
+
+	private static void printCountOfRepeatedCharsInString() {
+		String s = "Hello, How are you";
+		
+		char ch = 'H';
+		long count = s.chars().filter(i->i==ch).count();
+		
+		System.out.println("Count of a character '" + ch + "' in the string '"+s+"' : " + count);
+		
+		Map<Character, Long> charCount = s.chars()
+		        .mapToObj(c -> (char) c) // Convert int stream to char stream
+		        .collect(Collectors.groupingBy(
+		                Function.identity(), // Group by the characters themselves
+		                Collectors.counting() // Count the occurrences of each character
+		        ));
+		
+		System.out.println("1) Count of all characters in the string '"+s+"' : " + charCount);
+ 
+		//OR
+		
+		Map<Character, Integer> counts = new HashMap<>();
+		s.chars()
+        .mapToObj(c -> (char) c) // Convert int stream to char stream
+		.forEach(chr -> counts.put(chr, counts.getOrDefault(chr, 0)+1));
+		
+		System.out.println("2) Count of all characters in the string '"+s+"' : " + counts);
+				
 	}
 
 	
@@ -55,9 +76,10 @@ public class Streams {
 	  
 	  List<String> countryList = countries.stream().flatMap(c -> c.stream()).toList(); 
 	  
-	  for(String s : countryList) {
-		  System.out.println("Streams.printCountOfCountries() : " + s);
-	  };
+		/*
+		 * for(String s : countryList) {
+		 * System.out.println("Streams.printCountOfCountries() : " + s); };
+		 */
 	  
       
       // Create a map to store counts
@@ -65,10 +87,10 @@ public class Streams {
 	  // Iterate through the list and count occurrences
 	  countryList.stream().forEach(s -> counts.put(s, counts.getOrDefault(s, 0) + 1));
 	  
-		/*
-		 * Map<String, Long> counts = countryList.stream()
-		 * .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
-		 */
+	/*
+	 * Map<String, Long> counts = countryList.stream()
+	 * .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+	 */
 	          
       counts.entrySet().stream()
       .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
